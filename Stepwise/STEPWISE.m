654f3d05cf8c44953313d35af5d3d27dd21c7bd1
Shumpei Ogawa <shmp0722@gmail.com>
2017-10-11 15:29:50 +0900

    updates

--- Stepwise/STEPWISE.m
+++ Stepwise/STEPWISE.m
@@ -12,12 +12,12 @@ HFA = xlsread(fullfile(pwd,'df_20171002_Turpin.xlsx'), 4);
 %% stepwise
 
 for ii = 1:12
-[b,se,pval,inmodel,stats,nextstep,history] = stepwisefit(HFA,CH(:, ii),...
-            'penter',0.05,'premove',0.10); 
+    [b,se,pval,inmodel,stats,nextstep,history] = stepwisefit(HFA,CH(:, ii),...
+        'penter',0.05,'premove',0.10);
 end
 % results
-% CH; 1 10 16 29 38 57 62 63 64 
-% CH2; 16 25 28 
+% CH; 1 10 16 29 38 57 62 63 64
+% CH2; 16 25 28
 % CH3; none
 % CH4; none
 % CH5; 55
@@ -45,22 +45,23 @@ xlabel 'N factors'
 ylabel 'eigen value'
 
 nFact= sum(e> 1);
+nGr = nFact-1;
 
-[Loadings1,specVar1,T,stats] = factoran(HFA,nFact-1);
+[Loadings1,specVar1,T,stats] = factoran(HFA, nGr);
 
 LC =Loadings1;
 
 %%
 % Sigma = cov(HFA);
-% 
+%
 % e = eig(Sigma);
-% 
+%
 % figure; hold on;
 % plot(flip(e),'-*')
 % plot([0,70],[1,1],'--')
 % xlabel 'N factors'
 % ylabel 'eigen value'
-% 
+%
 % [LoadingsCov,specVarCov] = ...
 %         factoran(Sigma,nFact-1,'Xtype','cov','rotate','none');
 % LC = LoadingsCov
@@ -70,5 +71,39 @@ gr = zeros(size(HFA,2),1);
 for ii =1 : size(HFA,2)
     gr(ii)= find( LC(ii,:)==max(LC(ii,:))) ;
 end
-%% 
-tp = readtable(fullfile(pwd, '10-2testpoint_displacement.xlsx'));
+%%  load 10-2 test point
+% tp = xlsread(fullfile(pwd, '10-2testpoint.xlsx'));
+tp = readtable(fullfile(pwd, '10-2testpoint.xlsx'));
+
+%% by color
+c = jet(nGr);
+figure;hold on;
+for ii =1 : size(HFA,2)
+    plot(tp.turpin_disp_x(ii), tp.turpin_disp_y(ii), 's',...
+        'MarkerFaceColor',c(gr(ii),:),'MarkerEdgeColor',c(gr(ii),:),...
+        'MarkerSize',12)
+end
+set(gca,'YLim', get(gca, 'XLim'))
+
+%%
+c = jet(nGr-1);
+figure
+subplot(1,2,1);hold on;
+for ii =1 : size(HFA,2)
+    plot(tp.x(ii), tp.y(ii), 's',...
+        'MarkerFaceColor',c(gr(ii),:),'MarkerEdgeColor',[0 0 0],... %c(gr(ii),:),...
+        'MarkerSize',12)
+end
+set(gca,'YLim', get(gca, 'XLim'))
+axis equal
+
+hold off;
+
+subplot(1,2,2); hold on;
+for ii =1 : size(HFA,2)
+    plot(tp.turpin_disp_x(ii), tp.turpin_disp_y(ii), 's',...
+        'MarkerFaceColor',c(gr(ii),:),'MarkerEdgeColor', [0 0 0],... %c(gr(ii),:),...
+        'MarkerSize',12)
+end
+set(gca,'YLim', get(gca, 'XLim'))
+axis equal
